b3796fe7dc895e516115ab960fb96b434c4e9a25
Velvet2045 <55654558+Velvet2045@users.noreply.github.com>
2021-09-13 00:11:48 +0900

    chap5 Templates Finished!

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user
+/users/logout -> Log out
+/users/edit -> Edit my profile
+/users/remove -> Remove my profile
+
+/videos/:id -> Watch a video
+/videos/:id/edit -> Edit a video
+/videos/:id/delete -> Delete a video
+/videos/upload -> Upload a video
--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
-
 const logger = morgan("dev");
 
-const handleHome = (req, res, next) => {
-  return res.send("I love middlewares");
-};
-
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
-};
-
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
