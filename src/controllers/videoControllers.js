94408fb8c6dc0d81c56bbebcf1ac639be9729c04
Velvet2045 <55654558+Velvet2045@users.noreply.github.com>
2021-09-16 00:12:51 +0900

    recap get and post use

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -45,3 +45,21 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 5,
+    comments: 2,
+    createdAt: "just now",
+    views: 1,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,17 @@
 import express from "express";
 import {
-  upload,
   watch,
   getEdit,
   postEdit,
-  deleteVideo,
+  getUpload,
+  postUpload,
 } from "../controllers/videoControllers.js";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)").get(watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
