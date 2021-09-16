2bd402ea57f1557f23a64008f8e21c70bc597d4d
Velvet2045 <55654558+Velvet2045@users.noreply.github.com>
2021-09-16 23:11:21 +0900

    add mongoose DB

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -17,7 +17,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
