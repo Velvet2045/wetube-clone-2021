f4465c2a945d74d91b65fa5af10b84b206e06023
Velvet2045 <55654558+Velvet2045@users.noreply.github.com>
2021-10-07 19:56:22 +0900

    add video 2

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -212,16 +212,15 @@ export const postChangePassword = async (req, res) => {
   req.session.user.password = user.password;
   return res.redirect("/users/logout");
 };
+
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res.status(404).render("404");
   }
-  const videos = await Video.find({ owner: user._id });
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
-    videos,
   });
 };
