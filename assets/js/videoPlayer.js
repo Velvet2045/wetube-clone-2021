0f8e56e49a96c7d153497d3d0f9ccb013ab85e1f
Velvet2045 <55654558+Velvet2045@users.noreply.github.com>
2021-10-08 20:25:51 +0900

    add volume ctrl

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/client/js/videoPlayer.js":
+/*!**************************************!*\
+  !*** ./src/client/js/videoPlayer.js ***!
+  \**************************************/
+/***/ (() => {
+
+eval("var video = document.querySelector(\"video\");\nvar playBtn = document.getElementById(\"play\");\nvar muteBtn = document.getElementById(\"mute\");\nvar time = document.getElementById(\"time\");\nvar volume = document.getElementById(\"volume\");\n\nvar handlePlayClick = function handlePlayClick(e) {\n  if (video.paused) {\n    video.play();\n  } else {\n    video.pause();\n  }\n\n  playBtn.innerText = video.muted ? \"Play\" : \"Pause\";\n};\n\nvar handlePause = function handlePause(e) {\n  return playBtn.innerText = \"Play\";\n};\n\nvar handlePlay = function handlePlay(e) {\n  return playBtn.innerText = \"Pause\";\n};\n\nvar handleMute = function handleMute(e) {\n  if (video.muted) {\n    video.muted = false;\n  } else {\n    video.muted = true;\n  }\n\n  muteBtn.innerText = video.muted ? \"Unmute\" : \"Mute\";\n};\n\nplayBtn.addEventListener(\"click\", handlePlayClick);\nmuteBtn.addEventListener(\"click\", handleMute);\n\n//# sourceURL=webpack://wetube/./src/client/js/videoPlayer.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/client/js/videoPlayer.js"]();
+/******/ 	
+/******/ })()
+;
